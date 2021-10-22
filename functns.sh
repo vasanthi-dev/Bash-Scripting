8c85a74117d6f0f88beeaf4a3ef54d5126df8291
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-22 00:25:33 -0400

    status of return command

--- functns.sh
+++ functns.sh
@@ -4,9 +4,11 @@
 function abc(){
   echo "Hello I am a function abc"
   echo "a in function = $a"
+  return
   b=30
 }
 # main program
 a=10
 abc
+echo "status of abc = $?"
 echo "b in main program = $b"
