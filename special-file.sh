2d6af02c85508ca8e73621bf95e1c42830451e03
vasanthi-dev <vasanthi2dev@gmail.com>
2021-10-21 21:10:08 -0400

    special variables

--- special-file.sh
+++ special-file.sh
@@ -2,9 +2,10 @@
 echo 0 = $0
 echo 1 = $1
 echo 2 = $2
-echo @ = $@
-#echo * = $*
-#echo # =$#
+
+echo "S = $*"
+echo "H = $#"
+echo "A = $@"
 
 
-#echo -e "your name : $1 /n your age : $2"
+echo -e "your name : $1 /n your age : $2"
